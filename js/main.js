be49adbb1436bc1b9fd8748f8c053f1f81deb850
amedeo <amedeopasanisi97@gmail.com>
2021-07-14 15:35:32 +0200

    fine esercizio

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var nome = prompt("Inserisci il tuo nome") + " ";
+
+var cognome = prompt("Inserisci il tuo cognome") + " ";
+
+var colore = prompt("Inserisci il tuo colore preferito") + " ";
+
+document.getElementById("password").innerHTML = nome + cognome + colore + 21
